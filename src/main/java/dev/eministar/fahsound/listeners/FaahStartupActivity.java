001b78fa67fff0bb9f6b7fbfa25a88eefbbe701e
Nael-Emin Ben Oun <benounnaelemin@gmail.com>
2026-03-05 17:50:51 +0100

    bump version to 1.0.3, update Java compatibility to 21, and enhance plugin description for FAH Failure Sound

--- src/main/java/dev/eministar/fahsound/listeners/FaahStartupActivity.java
+++ src/main/java/dev/eministar/fahsound/listeners/FaahStartupActivity.java
@@ -2,18 +2,21 @@ package dev.eministar.fahsound.listeners;
 
 import com.intellij.execution.ExecutionManager;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.startup.StartupActivity;
+import com.intellij.openapi.startup.ProjectActivity;
 import com.intellij.task.ProjectTaskListener;
 import com.intellij.util.messages.MessageBusConnection;
+import kotlin.Unit;
+import kotlin.coroutines.Continuation;
 import org.jetbrains.annotations.NotNull;
 
-public final class FaahStartupActivity implements StartupActivity.DumbAware {
+public final class FaahStartupActivity implements ProjectActivity {
     @Override
-    public void runActivity(@NotNull Project project) {
+    public Object execute(@NotNull Project project, @NotNull Continuation<? super Unit> continuation) {
         FaahExternalSystemFailureWatcher.getInstance();
         FaahBuildEventWatcher.install(project);
         MessageBusConnection connection = project.getMessageBus().connect(project);
         connection.subscribe(ProjectTaskListener.TOPIC, new FaahProjectTaskFailureListener(project));
         connection.subscribe(ExecutionManager.EXECUTION_TOPIC, new FaahExecutionFailureListener(project));
+        return Unit.INSTANCE;
     }
 }
